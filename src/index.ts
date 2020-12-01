bb61d0b76b5caaed4a131676d4212662628cc0c5
Abdullah Kocakaya <40685751+akocakaya@users.noreply.github.com>
2020-12-01 21:34:09 +0300

    feat: implement eslint, prettier and husky (#3)
    * feat: implement eslint, prettier and husky
    
    * feat: add end of line eslint config
    
    * feat: add new line config

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,33 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
-import task from'./modules/task';
+import task from './modules/task';
 
 const bootServer = async () => {
-    const app: express.Application = express();
-    const port: number = 3000
-    app.use(bodyParser.json());
+  const app: express.Application = express();
+  const port = 3000
+  app.use(bodyParser.json());
 
-    try {
-        await mongoose.connect('mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false/task-db', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
-        console.log('MongoDB connected')
-    } catch (e) {
-        throw new Error(e)
-    }
-    
-    app.get('/', (req, res) => {
-      res.send('Hello World!')
+  try {
+    await mongoose.connect('mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false/task-db', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     })
-    
-    app.use('/api/task', task)
-    
-    app.listen(port, () => {
-      console.log(`Example app listening at http://localhost:${port}`)
-    })
+    console.log('MongoDB connected')
+  } catch (e) {
+    throw new Error(e)
+  }
+
+  app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+  app.use('/api/task', task)
+
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
 }
 
 
-bootServer()
-
-
+bootServer();
--- src/modules/task/task.model.ts
+++ src/modules/task/task.model.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 const { Schema, model } = mongoose;
 
 const taskSchema = new Schema({
-  text:  String,
+  text: String,
   author: String,
 }, { timestamps: true });
 
