f079f546322746d56afec7564f3fb7cae7a1bfe4
Onur Iltan <onur.iltan@gmail.com>
2020-12-03 22:13:18 +0300

    fix: make eslint work with prettier (#4)
    * feat: implement eslint, prettier and husky
    
    * feat: add end of line eslint config
    
    * fix: make eslint work with prettier
    
    * fix: task api route value
    
    Co-authored-by: akocakaya <abdullahkocakaya4@gmail.com>

--- src/modules/task/index.ts
+++ src/modules/task/index.ts
@@ -4,33 +4,34 @@ const router = express.Router();
 
 router.post('/', async (req, res) => {
   const { text, author } = req.body;
-  let newTask: any = ""
+  let newTask: any = '';
   try {
     newTask = await new taskModel({
-      text, author
-    }).save()
+      text,
+      author,
+    }).save();
   } catch (e) {
     console.log('e -> ', e);
-    res.status(400).send('error accured while creating task')
+    res.status(400).send('error accured while creating task');
   }
 
   res.status(201).send(newTask);
-})
+});
 
 router.get('/', (req, res) => {
   const { id } = req.query;
   res.status(201).send(`your ${id} task`);
-})
+});
 
 router.get('/getAll', async (req, res) => {
-  let tasks: any = []
+  let tasks: any = [];
   try {
-    tasks = await taskModel.find({}).lean()
+    tasks = await taskModel.find({}).lean();
   } catch (e) {
     console.log('e -> ', e);
-    res.status(400).send('error accured while getting tasks')
+    res.status(400).send('error accured while getting tasks');
   }
   res.status(201).send(tasks);
-})
+});
 
 export default router;
--- src/modules/task/task.model.ts
+++ src/modules/task/task.model.ts
@@ -1,10 +1,13 @@
 import mongoose from 'mongoose';
 const { Schema, model } = mongoose;
 
-const taskSchema = new Schema({
-  text: String,
-  author: String,
-}, { timestamps: true });
+const taskSchema = new Schema(
+  {
+    text: String,
+    author: String,
+  },
+  { timestamps: true },
+);
 
 const taskModel = model('task', taskSchema);
 
