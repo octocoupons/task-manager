e506438c703d4788651d0fd25b5d51263e5b8cbc
onuriltan <onur.iltan@gmail.com>
2020-12-16 23:25:26 +0100

    test: do not collect coverage under config folder

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   clearMocks: true,
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
   roots: ['<rootDir>/src'],
+  coveragePathIgnorePatterns: ['<rootDir>/src/config'],
   testEnvironment: 'node',
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
