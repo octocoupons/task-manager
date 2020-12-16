8b663055e928ed12777ae7e3b25558ee6b89eaca
akocakaya <abdullahkocakaya4@gmail.com>
2020-12-17 00:00:35 +0300

    wip: current implementation logger

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,30 @@ import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import taskRouter from './modules/task/router/task.router';
 
+import * as winston from 'winston';
+import * as expressWinston from 'express-winston';
+import debug from 'debug';
+
+const debugLog: debug.IDebugger = debug('app');
+
 const bootServer = async () => {
   const app: express.Application = express();
   const port = 5000;
   app.use(bodyParser.json());
+
+  app.use(
+    expressWinston.logger({
+      transports: [new winston.transports.Console()],
+      format: winston.format.combine(winston.format.colorize(), winston.format.json()),
+    }),
+  );
+
+  app.use(
+    expressWinston.errorLogger({
+      transports: [new winston.transports.Console()],
+      format: winston.format.combine(winston.format.colorize(), winston.format.json()),
+    }),
+  );
 
   try {
     await mongoose.connect('mongodb://localhost:27017/task-db', {
@@ -14,6 +34,7 @@ const bootServer = async () => {
       useUnifiedTopology: true,
     });
     console.log('MongoDB connected');
+    debugLog('MongoDB connected');
   } catch (e) {
     throw new Error(e);
   }
@@ -24,7 +45,7 @@ const bootServer = async () => {
 
   app.use('/api/task', taskRouter);
   app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    debugLog(`Example app listening at http://localhost:${port}`);
   });
 };
 
