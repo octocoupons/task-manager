24dc394bcd5599b36a174583d03c3a4a164f6c23
onuriltan <onur.iltan@gmail.com>
2020-12-29 20:30:17 +0100

    feat: configure jest to ignore certain files

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,13 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
   clearMocks: true,
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['src/**/*.ts', '!src/**/config/**', '!src/**/router/*.router.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/config/**',
+    '!src/**/*.router.ts',
+    '!src/**/*.validation.ts',
+    '!src/**/*.entity.ts',
+  ],
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
   testEnvironment: 'node',
   transform: {
